ca653f534faa4ccdcfddfb431cd908bcf83cd0a7
Denys <kanedius@gmail.com>
2022-05-15 13:15:20 +0300

    add

---
+++ src/components/App.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import Form from './Form/Form'
+import ContactList from './Contacts/Contacts'
+import Filter from './Filter/Filter'
+import s from './Form/Form.module.css';
+export class App extends React.Component{
+  state = {
+    contacts: [
+    ],
+    filter: '',
+  }
+ 
+submitHandler = data =>{
+  const flag = this.state.contacts.find(contact => contact.name === data.name)
+  this.setState(prevState =>{
+   return  flag?alert(`${data.name} is alredy in contacts`): {contacts :[data, ...prevState.contacts]}
+    
+     
+  })
+}
+chengeFilter = e=>{
+  this.setState({filter: e.currentTarget.value})
+}
+deleteContacts = id =>{
+  this.setState(prevState =>({
+    contacts: prevState.contacts.filter(contact => contact.id !== id)
+  }));
+}
+componentDidMount(){
+  const contacts = localStorage.getItem(`contacts`)
+  const contactsParse = JSON.parse(contacts)
+  if (contactsParse) {
+    this.setState({contacts:contactsParse})
+  }
+  
+}
+
+componentDidUpdate(prevProps,prevState){
+  if (prevState.contacts !== this.state.contacts) {
+    localStorage.setItem(`contacts`,JSON.stringify(this.state.contacts))
+    
+  }
+
+}
+
+    render(){
+      const normalizedFilter= this.state.filter.toLowerCase()
+      const visibleContacts = this.state.contacts.filter(contact =>contact.name.toLowerCase().includes(normalizedFilter) )
+    return (<div className={s.wrap}>
+      <h2>Phonebook</h2>
+      <Form onSubmit={this.submitHandler} />
+      <h2>Contacts</h2>
+      <ContactList contacts={visibleContacts} deleteContacts={this.deleteContacts}>
+      <Filter value={this.state.filter} onChange ={this.chengeFilter}/>
+      </ContactList>
+    </div>)
+    
+  }
+  
+}
